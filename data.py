ec6496db096c453756e1208a8038531bfc9a0560
Eviatar Bach <eviatarbach@gmail.com>
2016-04-15 02:37:56 -0700

    update

---
+++ data.py
@@ -0,0 +1,59 @@
+import pickle
+
+import numpy
+import scipy
+import scipy.interpolate
+import mpmath
+
+RGI_REGIONS = ['Alaska', 'AntarcticSubantarctic', 'ArcticCanadaNorth',
+               'ArcticCanadaSouth', 'CaucasusMiddleEast', 'CentralAsia',
+               'CentralEurope', 'GreenlandPeriphery', 'Iceland', 'LowLatitudes',
+               'NewZealand', 'NorthAsia', 'RussianArctic', 'Scandinavia',
+               'SouthAsiaEast', 'SouthAsiaWest', 'SouthernAndes', 'Svalbard',
+               'WesternCanadaUS']
+
+RGI_NAMES = ['Alaska', 'Antarctic and Subantarctic', 'Arctic Canada (North)',
+             'Arctic Canada (South)', 'Caucasus and Middle East',
+             'Central Asia', 'Central Europe', 'Greenland (periphery)',
+             'Iceland', 'Low Latitudes', 'New Zealand', 'North Asia',
+             'Russian Arctic', 'Scandinavia', 'South Asia (East)',
+             'South Asia (West)', 'Southern Andes', 'Svalbard and Jan Mayen',
+             'Western Canada and USA']
+
+a = 1/3.
+q = 5/3.
+gamma = 1.25
+
+P = pickle.load(open('P.p', 'rb'))
+V = pickle.load(open('V.p', 'rb'))
+P2 = pickle.load(open('P2.p', 'rb'))
+V2 = pickle.load(open('V2.p', 'rb'))
+
+f1 = scipy.interpolate.interp1d(P, V)
+f2 = scipy.interpolate.interp1d(P2, V2)
+
+alpha = 4/5.
+delta = 7/5.
+
+def F(P, V): return -V**(alpha)*P - V**(delta) + V
+
+def f(P):
+    if P > f2.x[-1]:
+        return 0.0
+    if f2.x[0] <= P <= f2.x[-1]:
+        return f2(P)
+    elif f1.x[0] <= P <= f1.x[-1]:
+        return f1(P)
+    else:
+        roots = numpy.roots([-1, 0, 1, -P, 0, 0, 0, 0])
+        return roots[numpy.nonzero(numpy.logical_and(numpy.isreal(roots), roots != 0))].real**5
+
+f_vec = numpy.vectorize(f)
+
+diff = numpy.vectorize(lambda p: mpmath.diff(lambda p2: f_vec(p2), p, h=1e-6))
+
+P0 = 0.384900179459750
+V0 = 0.06415
+
+def latex_bars(names, values):
+    return '\n'.join(map(lambda n, l: n + ' & \mybar{' + '{:.2f}'.format(l) + '}\\\\', names, values))
---
+++ load_lapse.py
@@ -0,0 +1,91 @@
+import pickle
+
+import numpy
+import scipy
+import scipy.stats
+import netCDF4
+
+def straddle(arr, value):
+    diffs = arr - value
+    diffs[diffs < 0] = float('inf')
+    index = numpy.argmin(diffs, axis=1)
+
+    # Boundary cases
+    index[value < arr[:, 0]] = 1
+    index[value > arr[:, -1]] = arr.shape[1] - 1
+    return numpy.vstack([index - 1, index]).T
+
+def closest_index_in_range(lower, upper, step, value):
+    '''
+    Find the index of the closest value to `value` in the range
+    [lower, lower + step, ..., upper - step, upper] in constant time. `upper`
+    must be greater than `lower`. If `value` is outside the range, return the
+    corresponding boundary index (0 or the last index). When two values are
+    equally close, the index of the smaller is returned.
+    '''
+    if value >= upper:
+        return int((upper - lower)/step)
+    elif value < lower:
+        return 0
+
+    value = value - lower
+    upper = upper - lower
+    lower = 0
+
+    index = int(value//step + 1)
+
+    if step*index - value >= value - step*(index - 1):
+        index -= 1
+
+    return index
+
+
+temp = netCDF4.Dataset('air.mon.mean.nc', 'a')  # monthly means of air temperatures
+height = netCDF4.Dataset('hgt.mon.mean.nc', 'a')  # geopotential heights
+
+# the -4 is to slice the years correctly
+temp_var = temp.variables['air'][-244:-4, :, :, :]
+height_var = height.variables['hgt'][-244:-4, :, :, :]
+
+all_glaciers = pickle.load(open('all_glaciers.p', 'br'))
+
+unique = set()
+
+for i,  glacier in enumerate(all_glaciers.index):
+    if i % 500 == 0:
+        print(i/len(all_glaciers))
+
+    glacier_lat_index = 72 - closest_index_in_range(-90, 90, 2.5, all_glaciers.loc[glacier, 'Location_y'])
+    lon = all_glaciers.loc[glacier, 'Location_x']
+
+    # conversion between 0 to 360 and -180 to 180 degree system
+    glacier_lon_index = (closest_index_in_range(-180, 180, 2.5, lon if lon < 180 else -(180 - (lon - 180))) + 72) % 144
+
+    unique.add((glacier_lat_index, glacier_lon_index))
+
+    grid_square_temps = temp_var[:, :, glacier_lat_index, glacier_lon_index]
+    grid_square_heights = height_var[:, :, glacier_lat_index, glacier_lon_index]
+
+    elev = all_glaciers.loc[glacier, 'Zmed']
+
+    #straddling = straddle(grid_square_heights, elev)
+    #elevs = grid_square_heights[:, straddling][:, 0]
+    #temps = grid_square_temps[:, straddling][:, 0]
+    #lapse_rates = -(temps[:, 1] - temps[:, 0])/(elevs[:, 1] - elevs[:, 0])
+
+    lapse_rates = []
+
+    for i in range(240):
+        heights = grid_square_heights[i, :]
+        temps = grid_square_temps[i, :]
+
+        index = heights < 10000
+        lapse_rates.append(-scipy.stats.linregress(heights[index], temps[index])[0])
+
+    lapse_mean = numpy.array(lapse_rates).reshape(20, 12).mean(axis=1).mean()
+
+    '''if (lapse_mean < 0) and (glacier[0] not in ['Alaska']):
+        from IPython.core.debugger import Tracer
+        Tracer()()'''
+
+    all_glaciers.loc[glacier, 'lapse_rate'] = lapse_mean
