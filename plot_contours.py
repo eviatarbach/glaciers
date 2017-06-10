0d7b8af321019e35be8d0203ae1357b6eeb7701e
Eviatar Bach <eviatarbach@gmail.com>
2017-06-10 04:49:17 -0700

    cleaning up old files, adding untracked plot files

---
+++ plot_contours.py
@@ -0,0 +1,77 @@
+import pandas
+import numpy
+from scipy.integrate import dblquad
+import scipy.stats
+
+import plot_config
+
+import matplotlib.pyplot as plt
+
+ERRS = {'g_abl': 0.005657,  # root-mean square error in interpolating g_abl
+        'g_acc': 0.002203}  # root-mean square error in interpolating g_acc
+
+glaciers = pandas.read_pickle('data/serialized/glaciers_climate')
+
+g_abl_pts = numpy.linspace(min(glaciers['g_abl']), max(glaciers['g_abl']), 25)
+g_acc_pts = numpy.linspace(min(glaciers['g_acc']), max(glaciers['g_acc']), 25)
+
+a = min(glaciers['g_acc']/glaciers['g_abl'])
+b = max(glaciers['g_acc']/glaciers['g_abl'])
+
+mu_x = numpy.mean(glaciers['g_acc'])
+mu_y = numpy.mean(glaciers['g_abl'])
+
+
+def f(mu_x, mu_y, x, y):
+    std_x, std_y = ERRS['g_acc'], ERRS['g_abl']
+    return 1/(2*numpy.pi*std_x*std_y)*numpy.exp(-1/2*((x - mu_x)/std_x)**2
+                                                - 1/2*((y - mu_y)/std_y)**2)
+
+
+X, Y = numpy.meshgrid(g_acc_pts, g_abl_pts)
+
+
+@numpy.vectorize
+def std_acc(g_acc, g_abl):
+    scaling = dblquad(lambda x, y: f(g_acc, g_abl, x, y), 0, numpy.inf,
+                      lambda y: a*y, lambda y: b*y)[0]
+    return numpy.sqrt(dblquad(lambda x, y: (x - g_acc)**2*f(g_acc, g_abl, x, y)/scaling, 0,
+                              numpy.inf, lambda y: a*y, lambda y: b*y)[0])
+
+
+@numpy.vectorize
+def std_abl(g_acc, g_abl):
+    scaling = dblquad(lambda x, y: f(g_acc, g_abl, x, y), 0, numpy.inf,
+                      lambda y: a*y, lambda y: b*y)[0]
+    return numpy.sqrt(dblquad(lambda x, y: (y - g_abl)**2*f(g_acc, g_abl, x, y)/scaling, 0,
+                              numpy.inf, lambda y: a*y, lambda y: b*y)[0])
+
+
+Z = std_acc(X, Y)
+cp = plt.contourf(X, Y, Z, cmap='Blues_r')
+cbar = plt.colorbar(cp)
+cbar.ax.tick_params(labelsize=14)
+fig = plt.gcf()
+fig.set_size_inches(10, 6)
+plt.xlabel('$\dot{g}_\mathrm{acc}$ $\left(y^{-1}\\right)$', fontsize=22)
+plt.ylabel('$\dot{g}_\mathrm{abl}$ $\left(y^{-1}\\right)$', fontsize=22, rotation=0, labelpad=45)
+plt.title('$\sqrt{E\left[(\dot{g}_\mathrm{acc} - \mu_{\dot{g}_\mathrm{acc}})^2\\right]}$',
+          fontsize=22, y=1.03)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.savefig('figures/contours_acc.pdf',  bbox_inches='tight')
+plt.clf()
+
+Z = std_abl(X, Y)
+cp = plt.contourf(X, Y, Z, cmap='Blues_r')
+cbar = plt.colorbar(cp)
+cbar.ax.tick_params(labelsize=14)
+fig = plt.gcf()
+fig.set_size_inches(10, 6)
+plt.xlabel('$\dot{g}_\mathrm{acc}$ $\left(y^{-1}\\right)$', fontsize=22)
+plt.ylabel('$\dot{g}_\mathrm{abl}$ $\left(y^{-1}\\right)$', fontsize=22, rotation=0, labelpad=45)
+plt.title('$\sqrt{E\left[(\dot{g}_\mathrm{abl} - \mu_{\dot{g}_\mathrm{abl}})^2\\right]}$',
+          fontsize=22, y=1.03)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.savefig('figures/contours_abl.pdf',  bbox_inches='tight')
---
+++ plot_slope_uncertainty.py
@@ -0,0 +1,41 @@
+import pandas
+import numpy
+import scipy.optimize
+
+import plot_config
+
+import matplotlib.pyplot as plt
+
+all_glaciers = pandas.read_pickle('data/serialized/all_glaciers')
+
+diff = all_glaciers['SLOPE_avg'] - all_glaciers['Slope']
+
+intervals = numpy.logspace(numpy.log10(all_glaciers['SLOPE_avg'].min()),
+                           numpy.log10(all_glaciers['SLOPE_avg'].max()), 50)
+
+mid = (intervals[:-1] + intervals[1:])/2
+err = numpy.zeros(len(mid))
+
+for i, _ in enumerate(intervals[:-1]):
+    mask = ((all_glaciers['SLOPE_avg'] > intervals[i])
+            & (all_glaciers['SLOPE_avg'] < intervals[i + 1]) & (diff > 0))
+    err[i] = numpy.sqrt((diff[mask]**2).mean())
+
+c, r = scipy.optimize.curve_fit(lambda slope, c, r: c*slope**r, xdata=mid[~numpy.isnan(err)],
+                                ydata=err[~numpy.isnan(err)])[0]
+
+plt.scatter(mid, err)
+
+slope = numpy.linspace(0, max(mid), 100)
+plt.plot(slope, c*slope**r)
+plt.xlabel(r'$\beta_H$', fontsize=22)
+plt.ylabel('RMSE', fontsize=20, rotation=0, labelpad=35)
+ax = plt.gca()
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.yaxis.set_ticks_position('left')
+ax.xaxis.set_ticks_position('bottom')
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+
+plt.savefig('figures/err.pdf', bbox_inches='tight')
